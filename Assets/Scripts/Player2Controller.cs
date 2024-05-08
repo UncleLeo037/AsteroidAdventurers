a13103bae33f4202f5d1dbf61978f209467e3848
UncleLeo037 <leo.muller@gmail.com>
2024-05-08 19:43:37 +0100

    subclass sandbox
    i think this counts

--- Assets/Scripts/Player2Controller.cs
+++ Assets/Scripts/Player2Controller.cs
@@ -2,26 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody2D))]
 
-public class Player2Controller : MonoBehaviour
+public class Player2Controller : PlayerController
 {
-    private Rigidbody2D player;
-    private BoxCollider2D coll;
-
-    [SerializeField] private LayerMask jumpableGround;
 
     // Start is called before the first frame update
     public void Start()
     {
-        player = GetComponent<Rigidbody2D>();
         coll = GetComponent<BoxCollider2D>();
+        player = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     public void Update()
     {
-        if (Input.GetButtonDown("Jump2") && IsGrounded())
+        if (Input.GetButtonDown("Jump2") && base.IsGrounded())
         {
             player.velocity = new Vector3(player.velocity.x, 10f, 0);
         }
@@ -29,10 +24,5 @@ public class Player2Controller : MonoBehaviour
 
         float dirX = Input.GetAxis("Horizontal2");
         player.velocity = new Vector2(dirX * 5f, player.velocity.y);
-    }
-
-    private bool IsGrounded()
-    {
-        return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, .1f, jumpableGround);
     }
 }
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+
+public class PlayerController : MonoBehaviour
+{
+    protected BoxCollider2D coll;
+    protected Rigidbody2D player;
+    [SerializeField] protected LayerMask jumpableGround;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    protected bool IsGrounded()
+    {
+        return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, .1f, jumpableGround);
+    }
+}
